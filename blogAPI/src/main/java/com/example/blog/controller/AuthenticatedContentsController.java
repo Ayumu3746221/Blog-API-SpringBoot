ba797066aa2ff1d571d2c6f06e8ef776dc931800
Ayumu3746221 <a.kkt0307@gmail.com>
2024-12-03 20:30:49 +0900

    create get all contents list

--- blogAPI/src/main/java/com/example/blog/controller/AuthenticatedContentsController.java
+++ blogAPI/src/main/java/com/example/blog/controller/AuthenticatedContentsController.java
@@ -7,18 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.blog.dto.PublishedContentListDTO;
+import com.example.blog.dto.AllContentListDTO;
 import com.example.blog.service.ContentService;
 
+
 @RestController
-@RequestMapping("/api/public/v1")
-public class PublishedContentsController {
-    
+@RequestMapping("/api/auth/v1")
+public class AuthenticatedContentsController {
+
     @Autowired
     private ContentService contentService;
 
-    @GetMapping("/published/articles")
-    public List<PublishedContentListDTO> getPublishedArticles() {
-        return contentService.getPublishedArticles();
+    @GetMapping("/authenticated/contents")
+    public List<AllContentListDTO> getAllContents() {
+        return contentService.getAllArticles();
     }
 }
--- blogAPI/src/main/java/com/example/blog/repository/ContentRepository.java
+++ blogAPI/src/main/java/com/example/blog/repository/ContentRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.example.blog.dto.AllContentListDTO;
 import com.example.blog.dto.PublishedContentListDTO;
 import com.example.blog.entity.Content;
 
@@ -14,4 +15,7 @@ public interface ContentRepository extends JpaRepository<Content, Integer>{
     
     @Query("SELECT new com.example.blog.dto.PublishedContentListDTO(c.contentId, c.title, c.imageUrl, c.contentUrl, c.updatedAt) FROM Content c WHERE c.isPublished = TRUE")
     List<PublishedContentListDTO> findPublishedArticles();
+
+    @Query("SELECT new com.example.blog.dto.PublishedContentListDTO(c.contentId, c.title, c.imageUrl, c.contentUrl, c.updatedAt, c.isPublished) FROM Content c")
+    List<AllContentListDTO> findAllArticles();
 }

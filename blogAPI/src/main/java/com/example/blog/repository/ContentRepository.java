1ec00800a9266143f8f767c3cc400a36e9fd342c
Ayumu3746221 <a.kkt0307@gmail.com>
2024-12-03 22:06:54 +0900

    Compatible with docker

--- blogAPI/src/main/java/com/example/blog/repository/ContentRepository.java
+++ blogAPI/src/main/java/com/example/blog/repository/ContentRepository.java
@@ -13,9 +13,9 @@ import com.example.blog.entity.Content;
 @Repository
 public interface ContentRepository extends JpaRepository<Content, Integer>{
     
-    @Query("SELECT new com.example.blog.dto.PublishedContentListDTO(c.contentId, c.title, c.imageUrl, c.contentUrl, c.updatedAt) FROM Content c WHERE c.isPublished = TRUE")
+    @Query(value = "SELECT new com.example.blog.dto.PublishedContentListDTO(c.contentId, c.title, c.imageUrl, c.contentUrl, c.updatedAt) FROM Content c WHERE c.isPublished = TRUE")
     List<PublishedContentListDTO> findPublishedArticles();
 
-    @Query("SELECT new com.example.blog.dto.PublishedContentListDTO(c.contentId, c.title, c.imageUrl, c.contentUrl, c.updatedAt, c.isPublished) FROM Content c")
+    @Query(value = "SELECT new com.example.blog.dto.AllContentListDTO(c.contentId, c.title, c.imageUrl, c.contentUrl, c.updatedAt, c.isPublished) FROM Content c")
     List<AllContentListDTO> findAllArticles();
 }

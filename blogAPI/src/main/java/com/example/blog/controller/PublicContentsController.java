5cdd4db9896bdbf6e0b7135e775e88dd16f8f46b
Ayumu3746221 <a.kkt0307@gmail.com>
2024-12-07 20:27:04 +0900

    create fetching content api

--- blogAPI/src/main/java/com/example/blog/controller/PublicContentsController.java
+++ blogAPI/src/main/java/com/example/blog/controller/PublicContentsController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.blog.dto.ArticleDTO;
 import com.example.blog.dto.PublishedContentListDTO;
 import com.example.blog.service.ContentService;
 
@@ -20,5 +22,10 @@ public class PublicContentsController {
     @GetMapping("/published/articles")
     public List<PublishedContentListDTO> getPublishedArticles() {
         return contentService.getPublishedArticles();
+    }
+
+    @GetMapping("/articles/{id}")
+    public ArticleDTO getArticleByContentId(@PathVariable("id") Integer contentId) {
+        return contentService.getArticleByContentId(contentId);
     }
 }
